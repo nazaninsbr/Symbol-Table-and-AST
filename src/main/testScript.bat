ea7e74983ff41ba007d0a346dac6b6545b9d8969
Marzieh <marziehrezaie2@gmail.com>
2018-11-20 21:16:23 +0330

    Update testScript.bat

--- src/main/testScript.bat
+++ src/main/testScript.bat
@@ -4,16 +4,16 @@ del *.java
 java org.antlr.v4.Tool Smoola.g4
 javac *.java
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/1.txt
-rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/2.txt
+rem  java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/2.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/3.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/4.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/5.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/6.txt
-rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/7.txt
+rem org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/7.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/8.txt
-rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/9.txt
-rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/10.txt
-rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/11.txt
+java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/9.txt
+java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/10.txt
+java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/11.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/12.txt
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/13.sml
 rem java org.antlr.v4.runtime.misc.TestRig Smoola program -gui < ./Tests/14.sml
