cba67fb62cade627d0e0aebbcec362e75d062257
Marzieh <marziehrezaie2@gmail.com>
2018-11-20 13:45:12 +0330

    set line

--- src/main/ast/node/Node.java
+++ src/main/ast/node/Node.java
@@ -5,11 +5,11 @@ import ast.Visitor;
 public abstract class Node {
 	int line_number; 
 
-	void set_line_number(int line_number){
+	public void set_line_number(int line_number){
 		this.line_number = line_number;
 	}
 
-	int get_line_number(){
+	public int get_line_number(){
 		return this.line_number;
 	}
 }
