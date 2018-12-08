e6ecde530d1acc8eba63ae733d3ad368626802a5
Nazanin Sabri <nazanin.sabrii@gmail.com>
2018-12-08 20:29:32 +0330

    fix return type check

--- src/main/ast/VisitorImpl.java
+++ src/main/ast/VisitorImpl.java
@@ -421,8 +421,10 @@ public class VisitorImpl implements Visitor {
 
     void check_method_return_type_conditions(MethodDeclaration methodDeclaration){
         methodDeclaration.getReturnValue().accept(this);
-        if (! methodDeclaration.getReturnType().toString().equals(methodDeclaration.getReturnValue().getType().toString())){
-            System.out.println("Line:"+Integer.toString(methodDeclaration.getReturnValue().get_line_number())+":"+methodDeclaration.getName().getName()+" return type must be "+methodDeclaration.getReturnType().toString());
+        if(! methodDeclaration.getReturnValue().getType().toString().equals("NOType")){
+            if (! methodDeclaration.getReturnType().toString().equals(methodDeclaration.getReturnValue().getType().toString())){
+                System.out.println("Line:"+Integer.toString(methodDeclaration.getReturnValue().get_line_number())+":"+methodDeclaration.getName().getName()+" return type must be "+methodDeclaration.getReturnType().toString());
+            }
         }
     }
 
